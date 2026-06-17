d87c30e1a3c8b89137baab72c8ab31904738b324
justinpaulware <77998303+justinpaulware@users.noreply.github.com>
2026-06-17 18:05:10 -0400

    layers.js

--- layers.js
+++ layers.js
@@ -1,7 +1,4 @@
 window.LAYERS = [
-  /* =====================================
-     ECONOMIC DEVELOPMENT
-  ===================================== */
 
   {
     id: "innovation-ecosystem",
@@ -12,7 +9,9 @@ window.LAYERS = [
     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSB7IiUS_Fmsu17q__RYqAMeDkFQpo45FxG1bTQmS234zWANLELF61wyUOsFT4ehEVym_mX5D9IIDDy/pub?output=csv",
 
     latField: "Latitude",
-    lngField: "Longitude"
+    lngField: "Longitude",
+
+    style: "innovation-points"
   },
 
   {
@@ -24,12 +23,10 @@ window.LAYERS = [
     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOmFRs9WqWbsV18n0Ulujr-yNGiyEfFmiDzKQEXBuDBh-Cak-ZQdhczddXWLnRQcDk_1dmAawjtqb6/pub?output=csv",
 
     latField: "Latitude",
-    lngField: "Longitude"
-  },
+    lngField: "Longitude",
 
-  /* =====================================
-     GEOGRAPHY
-  ===================================== */
+    style: "opportunity-points"
+  },
 
   {
     id: "counties",
@@ -39,7 +36,9 @@ window.LAYERS = [
 
     url: "https://services6.arcgis.com/EbVsqZ18sv1kVJ3k/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/2/query?where=NAME='Westchester'%20OR%20NAME='Rockland'%20OR%20NAME='Sullivan'%20OR%20NAME='Putnam'%20OR%20NAME='Ulster'%20OR%20NAME='Orange'%20OR%20NAME='Dutchess'&outFields=*&f=geojson",
 
-    initialExtentLayer: true
+    initialView: true,
+
+    style: "county-outline"
   },
 
   {
@@ -48,7 +47,9 @@ window.LAYERS = [
     group: "Geography",
     type: "rest",
 
-    url: "https://services6.arcgis.com/EbVsqZ18sv1kVJ3k/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/6/query?where=COUNTY='Westchester'%20OR%20COUNTY='Rockland'%20OR%20COUNTY='Sullivan'%20OR%20COUNTY='Putnam'%20OR%20COUNTY='Ulster'%20OR%20COUNTY='Orange'%20OR%20COUNTY='Dutchess'&outFields=*&f=geojson"
+    url: "https://services6.arcgis.com/EbVsqZ18sv1kVJ3k/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/6/query?where=COUNTY='Westchester'%20OR%20COUNTY='Rockland'%20OR%20COUNTY='Sullivan'%20OR%20COUNTY='Putnam'%20OR%20COUNTY='Ulster'%20OR%20COUNTY='Orange'%20OR%20COUNTY='Dutchess'&outFields=*&f=geojson",
+
+    style: "county-outline"
   }
 
 ];
